a82f87a25a037a63b7ca90bff21ae3a7917f548c
ngamelie <ng_nhn@yahoo.ca>
2022-09-19 09:49:11 -0400

    add header and footer

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+
+// From routes/Product.js
+// GET -  localhost:3001/product/ 
+// POST-  localhost:3001/product/ 
+
+ const productRouter = require("./routes/Product");
+ 
+ app.use("/product", productRouter);
 
 app.listen(3001, () => {
   console.log("Server running on port 3001");
