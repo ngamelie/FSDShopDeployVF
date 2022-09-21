c7de32723551db72415c152e5c7e7da434cb7a53
zeenw <111456142+zeenw@users.noreply.github.com>
2022-09-21 12:21:53 -0400

    Update Home.js

--- client/src/components/page/Home.js
+++ client/src/components/page/Home.js
@@ -26,7 +26,7 @@ function Home(props) {
         setRsLength(response.data.length)
         });
 
-      axios.get(PATH + "/product/name/" + key + "_1").then((response) => {
+      axios.get(PATH + "/product/name/" + key + "_/_1").then((response) => {
         setProductList(response.data);
         });
 
@@ -52,7 +52,7 @@ function Home(props) {
           setRsLength(response.data.length)
           });
   
-        axios.get(PATH + "/product/name/" + key + "_" + pageNumber).then((response) => {
+        axios.get(PATH + "/product/name/" + key + "_/_" + pageNumber).then((response) => {
           setProductList(response.data);
           });
 
@@ -70,55 +70,64 @@ function Home(props) {
 
 
   return (
-    <div className="homecontainer">
-      <div className="banner">
-        <img src={require("../../asset/images/home.jpg")}  />
+    <div className="homecontainer-fluid row">
+
+
+      <div className="col-2">
+        <br /><br />
+          <h5>Electronics</h5>
       </div>
-      <div className="container container-fluid">
-        <section id="products" className="container mt-5">
-          <div className="row">
-            {productList.map((item, key) => (
-              <div className="col-sm-12 col-md-6 col-lg-3 my-3" >
-                <div className="card p-3 rounded" key={item.id}>
-                  <img
-                    className="card-img-top mx-auto"
-                    src={require(`../../asset/images/${item.img}.jpg`)}
-                  />
-                  <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">
-                      <Link to={`/product/${item.pid}`}>{item.description}</Link>
-                    </h5>
-                    <StarRatings
-                      rating={item.rate}
-                      numberOfStars={5}
-                      starRatedColor="#febd69"
-                      starDimension={20} 
-                      starSpacing={1}                                    
+
+      <div className="col-10">
+        <div className="banner">
+          <img src={require("../../asset/images/home.jpg")}  />
+        </div>
+        <div className="container container-fluid">
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {productList.map((item, key) => (
+                <div className="col-sm-12 col-md-6 col-lg-3 my-3" >
+                  <div className="card p-3 rounded" key={item.id}>
+                    <img
+                      className="card-img-top mx-auto"
+                      src={require(`../../asset/images/${item.img}.jpg`)}
                     />
-                    <p className="card-text mt-3">${item.price}</p>
-                    
-                  </div>
-                  <div className="btnview">
-                    <Link to={`/product/${item.pid}`}><span id="view_btn">View Details</span></Link>                    
+                    <div className="card-body d-flex flex-column">
+                      <h5 className="card-title">
+                        <Link to={`/product/${item.pid}`}>{item.description}</Link>
+                      </h5>
+                      <StarRatings
+                        rating={item.rate}
+                        numberOfStars={5}
+                        starRatedColor="#febd69"
+                        starDimension={20} 
+                        starSpacing={1}                                    
+                      />
+                      <p className="card-text mt-3">${item.price}</p>
+                      
+                    </div>
+                    <div className="btnview">
+                      <Link to={`/product/${item.pid}`}><span id="view_btn">View Details</span></Link>                    
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+          </section>
+          <div className="d-flex justify-content-center mt-5">
+            <Pagination 
+              activePage = {currentPage}
+              itemsCountPerPage = {4}
+              totalItemsCount = {rsLength}
+              onChange = {setCurrentpageNo}
+              nextPageText = {"Next"}
+              prevPageText = {"Prev"}
+              firstPageText = {"First"}
+              lastPageText = {"Last"}
+              itemClass = "page-item"
+              linkClass= "page-link"
+            />
           </div>
-        </section>
-        <div className="d-flex justify-content-center mt-5">
-          <Pagination 
-            activePage = {currentPage}
-            itemsCountPerPage = {4}
-            totalItemsCount = {rsLength}
-            onChange = {setCurrentpageNo}
-            nextPageText = {"Next"}
-            prevPageText = {"Prev"}
-            firstPageText = {"First"}
-            lastPageText = {"Last"}
-            itemClass = "page-item"
-            linkClass= "page-link"
-          />
         </div>
       </div>
     </div>
