3c734aac0101a37615aaa5efddd4da18dc367570
zeenw <111456142+zeenw@users.noreply.github.com>
2022-09-26 19:44:17 -0400

    btn

--- client/src/components/page/Home.js
+++ client/src/components/page/Home.js
@@ -82,7 +82,7 @@ function Home() {
 
           {catergories.map((category) => (
             <div>
-              <a href={`/product/category/${category.cid}`}>
+              <a className="btn" href={`/product/category/${category.cid}`}>
                 {" "}
                 {category.title}{" "}
               </a>{" "}
